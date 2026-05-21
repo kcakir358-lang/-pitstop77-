84fbf4a5b081c0cad906a05832d9c1684b43897c
Koray <mailadresin@gmail.com>
2026-05-21 18:21:29 +0300

    google verification

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,10 +1,12 @@
 import type { Metadata } from "next";
 import "./globals.css";
-
-export const metadata: Metadata = {
-  title: "Pitstop77",
+export const metadata = {
+  title: "PITSTOP77",
   description: "Mobil oto bakım servisi",
-};
+  verification: {
+    google: "NlsE53lTOvE5b0UexWXGoWHw_Bb_44eTjvU_PrIPasA",
+  },
+}
 
 export default function RootLayout({
   children,
