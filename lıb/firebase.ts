06a3660274babb56031db21a08f28561033ff7e0
Koray <mailadresin@gmail.com>
2026-05-27 21:11:05 +0300

    fix page firebase import

--- lıb/firebase.ts
+++ lıb/firebase.ts
@@ -1,15 +1,19 @@
-import { initializeApp } from "firebase/app";
+import { initializeApp, getApps, getApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBwNzCNF3RRvlc0MdBhRh4Z4471tKOgJCI",
-  authDomain: "pitstop77-e1074.firebaseapp.com",
-  projectId: "pitstop77-e1074",
-  storageBucket: "pitstop77-e1074.firebasestorage.app",
-  messagingSenderId: "238971614624",
-  appId: "1:238971614624:web:61f80396faad54f67620c5",
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId:
+    process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
 };
 
-const app = initializeApp(firebaseConfig);
+const app =
+  getApps().length === 0
+    ? initializeApp(firebaseConfig)
+    : getApp();
 
 export const db = getFirestore(app);
